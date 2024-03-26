57de2ae40a43dd9ab274b50d858a896fe2e35ebe
Ojan <164023597+WP2-FauzanNouvalA@users.noreply.github.com>
2024-03-26 21:12:40 +0700

    pustaka-booking
    ojan

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function Index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "Nama Saya Fauzan Nouval Kuliah Di B ES I";
+    }
+}
